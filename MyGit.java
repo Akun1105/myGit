ec8485972b4e229bc6796493e9043b09639792da
Akun1105 <hu.bing_wk@aliyun.com>
2017-03-21 17:53:20 +0800

    modify msg

--- MyGit.java
+++ MyGit.java
@@ -2,7 +2,8 @@ public class MyGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub	
-		System.out.println("Hello Git,I'm coming!");
+		String msg = "Hello Git,I'm coming!";
+		System.out.println(msg);
 	}
 
 }
